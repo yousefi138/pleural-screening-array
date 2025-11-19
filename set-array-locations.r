e2e134a7531d3724a3071916bd03566e27da3f0a
Paul Yousefi <paul.yousefi@bristol.ac.uk>
2025-11-19 13:51:51 +0000

    re-randomising with replacement spotlight samples

--- set-array-locations.r
+++ set-array-locations.r
@@ -1,14 +1,14 @@
-paths <- list(project = "~/repos/pleural-screening-array")
-paths$data <- file.path(paths$project, "data")
-paths$output <- file.path(paths$project, "results")
+# set dirs  
+dir <- paths
 
 library(readxl)
 
 # pid file with patient info
-file <- file.path(paths$data, "Patient IDs - Epigenetics 09-2025 with diagnosis information.xlsx")
+file <- file.path(dir$data, 
+        "Copy of Patient IDs - Epigenetics 09-2025 with diagnosis information_with_spotlight.xlsx")
 
 # old pid file
-#file <- file.path(paths$data, "Patient IDs - Epigenetics 09-2025.xlsx")
+#file <- file.path(dir$data, "Patient IDs - Epigenetics 09-2025.xlsx")
 #identical(pids$patient.id, new$patient.id)
 #[1] TRUE
 
@@ -37,7 +37,7 @@ plate <- plate[,c("plate", "row", "col", "well")]
 
 ## ----randomize and combine ----------------------------------------------
 # randomize 
-set.seed(2749)
+set.seed(274239)
 idx <- sample(1:nrow(pids))
 
 out <- cbind(pids[idx,], plate)
@@ -56,50 +56,54 @@ with(out, table(sex, plate))
 ##   Male   56 62 69 25
 
 with(out, table(final.diagnosis.1, plate))
-##                       plate
-##final.diagnosis.1        1  2  3  4
-##  M/breast               9  7  5  5
-##  M/colorectal           0  0  2  1
-##  M/endometrial          0  0  0  1
-##  M/haem                 6  6  5  2
-##  M/lung/adeno          11 15  6  8
-##  M/lung/small           1  2  1  0
-##  M/lung/squamous        0  4  1  0
-##  M/lung/x               0  0  1  0
-##  M/meso/biphasic        1  0  5  0
-##  M/meso/desmoplastic    0  0  1  0
-##  M/meso/epithelioid    10 16 11  6
-##  M/meso/sarcomatoid     0  2  3  0
-##  M/meso/unclassed       1  0  0  0
-##  M/meso/x               2  0  3  1
-##  M/Mullerian            0  2  0  1
-##  M/ovarian              2  1  1  2
-##  M/pancreatic           0  0  0  2
-##  M/renal                0  0  2  0
-##  M/sarcoma              0  1  0  1
-##  M/skin                 0  1  0  0
-##  M/thymus               0  1  0  0
-##  M/UGI                  0  2  0  0
-##  X/cardiac/mixed        0  1  0  0
-##  X/cardiac/u           13  5 22  4
-##  X/hepatic/chronic      0  0  1  0
-##  X/hepatic/u            5  4  3  1
-##  X/infection/bacterial  2  1  8  0
-##  X/infection/SPE        4  5  4  2
-##  X/infection/utrue      7  3  2  3
-##  X/inflam/BAPE         11  8  6  4
-##  X/inflam/CTD           1  1  0  0
-##  X/inflam/drug          0  2  1  0
-##  X/inflam/NSP           5  2  1  0
-##  X/inflam/other         0  0  0  1
-##  X/inflam/pancreatitis  0  1  0  0
-##  X/inflam/polysero      1  1  0  0
-##  X/inflam/trauma        1  0  1  1
-##  X/inflam/viral         0  0  0  1
-##  X/renal/u              3  2  0  1
+#                       plate
+#final.diagnosis.1        1  2  3  4
+#  M/breast               6  8  8  5
+#  M/colorectal           1  1  1  0
+#  M/endometrial          0  1  0  0
+#  M/haem                 6  6  6  2
+#  M/lung/adeno          18 14  6  5
+#  M/lung/small           2  1  1  0
+#  M/lung/squamous        1  2  1  1
+#  M/lung/x               0  0  1  0
+#  M/meso/biphasic        1  1  4  0
+#  M/meso/desmoplastic    0  0  0  1
+#  M/meso/epithelioid    16 13  9  7
+#  M/meso/sarcomatoid     1  2  2  0
+#  M/meso/unclassed       0  0  0  1
+#  M/meso/x               3  1  2  0
+#  M/Mullerian            1  2  1  0
+#  M/ovarian              3  0  3  0
+#  M/pancreatic           1  1  0  0
+#  M/renal                1  0  1  0
+#  M/sarcoma              1  0  0  1
+#  M/skin                 1  0  0  0
+#  M/thymus               0  0  1  0
+#  M/UGI                  0  1  1  0
+#  X/cardiac/chronic      0  0  1  0
+#  X/cardiac/constrict    1  0  0  0
+#  X/cardiac/mixed        0  0  0  1
+#  X/cardiac/u            9 13 13  8
+#  X/cardicac/valvular    0  1  0  0
+#  X/hepatic/u            2  4  4  1
+#  X/infection/bacterial  0  4  6  1
+#  X/infection/CPPE       0  0  0  1
+#  X/infection/SPE        2  5  4  2
+#  X/infection/utrue      4  3  3  3
+#  X/inflam/BAPE          5  4  8  6
+#  X/inflam/CTD           0  1  1  0
+#  X/inflam/drug          0  2  1  0
+#  X/inflam/NSP           4  3  1  1
+#  X/inflam/other         1  0  0  0
+#  X/inflam/pancreatitis  0  0  1  0
+#  X/inflam/polysero      1  0  1  0
+#  X/inflam/trauma        1  0  2  0
+#  X/inflam/viral         0  0  1  0
+#  X/reactive/subphrenic  0  0  0  1
+#  X/renal/u              3  2  1  0
 
 ## ----write ---------------------------------------------------------
 data.table::fwrite(out, 
-                file.path(paths$output,
+                file.path(dir$output,
                     paste0(format(Sys.Date(), "%Y%m%d"),
                     "-pleural-array-locations.csv")))
